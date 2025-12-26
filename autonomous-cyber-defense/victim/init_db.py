c0a5edfefef53a053de22090ce4080f4471ff9c9
Nezheg0l <devochka.wen.1488@gmail.com>
2025-12-26 11:10:35 +0200

    Clean up comments in init_db function
    Removed comments for user and ticket tables in init_db function.

--- autonomous-cyber-defense/victim/init_db.py
+++ autonomous-cyber-defense/victim/init_db.py
@@ -4,15 +4,12 @@ def init_db():
     conn = sqlite3.connect('bank.db')
     c = conn.cursor()
     
-    # 1. Таблиця Юзерів
     c.execute('''CREATE TABLE IF NOT EXISTS users 
                  (id INTEGER PRIMARY KEY, username TEXT, password TEXT, balance REAL)''')
     
-    # 2. Таблиця Тікетів (для XSS)
     c.execute('''CREATE TABLE IF NOT EXISTS tickets 
                  (id INTEGER PRIMARY KEY, content TEXT)''')
 
-    # Додаємо адміна і жертву
     c.execute("INSERT OR IGNORE INTO users (id, username, password, balance) VALUES (1, 'admin', 'SuperSecretPass123', 1000000)")
     c.execute("INSERT OR IGNORE INTO users (id, username, password, balance) VALUES (2, 'user', '123456', 50)")
     
